9097a7e823465ec478f39ec8c98c2d8d4006d83d
frisbee09 <frisbee09@users.noreply.github.com>
2020-04-29 11:47:07 +0100

    and another tweak

--- webpack-deploy.config.js
+++ webpack-deploy.config.js
@@ -2,12 +2,22 @@ const merge = require("webpack-merge");
 const common = require("./webpack.common");
 const nodeExternals = require("webpack-node-externals");
 
-module.exports = merge(common, {
+const sharedConfig = merge(common, {
   mode: "production",
-  entry: { client: "./client/index.tsx", server: "./server/index.ts" },
   output: {
     filename: "[name].js",
     path: __dirname + "/dist",
   },
+});
+
+const serverConfig = merge(sharedConfig, {
+  target: "node",
+  entry: { server: "./server/index.ts" },
   externals: [nodeExternals()],
 });
+
+const clientConfig = merge(sharedConfig, {
+  entry: { client: "./client/index.tsx" },
+});
+
+module.exports = [serverConfig, clientConfig];
